bbbf0fd8778fb5dab668dc2ef534887e05bfcb96
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-01-26 00:05:12 +0800

    card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,9 @@
-import Footer from "./Footer";
-import Header from "./Header";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Card />
     </>
   );
 }
