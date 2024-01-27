831e2dad319acf350ff8ff3f2dbc87c3bd035014
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-01-27 21:50:58 +0800

    push about props

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function list(props) {
+  const category = props.category;
+  const itemList = props.items;
+
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; 
+      <b>{item.calories}</b>
+    </li>
+  ));
+
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
+}
