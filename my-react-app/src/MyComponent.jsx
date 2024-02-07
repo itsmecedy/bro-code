69c80b6eccd256b892c4acbb5af38366e34bb0ac
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-08 00:02:26 +0800

    --

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -11,10 +11,8 @@ export default function MyComponent() {
   function handleRemoveFood(index) {
     setFoods(foods.filter((_, i) => i !== index));
   }
-
   return (
     <div>
-      {" "}
       <h2>List of Food</h2>
       <ul>
         {foods.map((food, index) => (
