fbe4186aa75b21e86556d5af42de8d0cefb6641f
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-07 23:48:26 +0800

    --

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -6,12 +6,12 @@ export default function MyComponent() {
   function handleAddFood() {
     const newFood = document.getElementById("foodInput").value;
     document.getElementById("foodInput").value = "";
-
     setFoods((f) => [...f, newFood]);
-    function handleRemoveFood(index) {
-      setFoods(foods.filter((_, i) => i !== index));
-    }
   }
+  function handleRemoveFood(index) {
+    setFoods(foods.filter((_, i) => i !== index));
+  }
+
   return (
     <div>
       {" "}
