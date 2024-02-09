baf783fc1ccc6e53569f45fcb8a81b2b5ae9a94c
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-10 01:10:39 +0800

    new learnings

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,28 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 
 export default function MyComponent() {
-  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
-  function handleAddFood() {
-    const newFood = document.getElementById("foodInput").value;
-    document.getElementById("foodInput").value = "";
-    setFoods((f) => [...f, newFood]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  function addCount() {
+    setCount((c) => c + 1);
   }
-  function handleRemoveFood(index) {
-    setFoods(foods.filter((_, i) => i !== index));
+  function subtractCount() {
+    setCount((c) => c - 1);
   }
+
   return (
     <div>
-      <h2>List of Food</h2>
-      <ul>
-        {foods.map((food, index) => (
-          <li key={index} onClick={() => handleRemoveFood(index)}>
-            {food}
-          </li>
-        ))}
-      </ul>
-      <input type="text" id="foodInput" placeholder="Enter food name" />
-      <button onClick={handleAddFood}> Add Food</button>
+      <p>Count: {count}</p>
+      <button onClick={addCount}>Add</button>
+      <button onClick={subtractCount}>Subtract</button>
     </div>
   );
 }
